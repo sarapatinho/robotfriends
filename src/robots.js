71eac33fb9fb97dabc1dfad7e561091f512a88ca
sarapatinho <sarapatinhorodrigues@gmail.com>
2020-01-24 17:51:32 +0000

    first commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Sara Rodrigues',
+		username: 'sara',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Joao Retre',
+		username: 'joao',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Sergio Pereira',
+		username: 'sergio',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Catarina Leote',
+		username: 'cat',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Jane Doe',
+		username: 'jane1',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Jane Doe',
+		username: 'jane2',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Jane Doe',
+		username: 'jane3',
+		email: 'exemplo@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Jane Doe',
+		username: 'jane4',
+		email: 'exemplo@gmail.com'
+	}
+]
